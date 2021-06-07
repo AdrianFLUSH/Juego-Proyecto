498fd95472c4e4c80c89e6531e6e0cfc625a7adb
EduardoGonzalezParra <eduardogp2203@gmail.com>
2021-06-07 08:36:33 -0500

    #5 Cambios button

--- Button.java
+++ Button.java
@@ -12,10 +12,7 @@ public abstract class Button extends Actor
      * Act - do whatever the Button wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
-    private boolean mouseOver = false;
-    private static int TRANS = 255;
-    
+
     public void act() 
     {
         checkClick();
